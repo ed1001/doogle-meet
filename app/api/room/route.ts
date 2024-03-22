fa2c8901427f0500762d758e82a9d2fbb51dba58
Edward Phillips <44973397+ed1001@users.noreply.github.com>
2024-03-22 13:10:17 +0100

    remove room api

--- app/api/room/route.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@/db/db";
-import { RoomsTable } from "@/db/schema";
-
-export async function GET() {
-  const dbRes = await db.select().from(RoomsTable);
-
-  return Response.json({ dbRes });
-}
