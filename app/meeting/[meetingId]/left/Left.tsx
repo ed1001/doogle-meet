618ea60477897658aecfc5bf4021043b7cdae67d
Edward Phillips <44973397+ed1001@users.noreply.github.com>
2024-04-29 16:16:28 +0200

    add styles to left meeting page

--- app/meeting/[meetingId]/left/Left.tsx
+++ app/meeting/[meetingId]/left/Left.tsx
@@ -1,15 +1,25 @@
-import { useMeetingContext } from "@/context/meeting/meeting-context";
 import { MeetingState } from "@/types";
+
+import { Button } from "@/components/Button";
+import { useMeetingContext } from "@/context/meeting/meeting-context";
+import Link from "next/link";
 
 export default function Left() {
   const { setMeetingState } = useMeetingContext();
 
   return (
-    <main className="flex h-lvh flex-col">
-      you left the chat
-      <button onClick={() => setMeetingState?.(MeetingState.LOBBY)}>
-        rejoin meeting
-      </button>
+    <main className="flex h-lvh flex-col items-center justify-center gap-7">
+      <h1 className="text-4xl">You left the meeting</h1>
+      <div className="flex gap-3">
+        <Button
+          outline
+          text="Rejoin meeting"
+          onClick={() => setMeetingState?.(MeetingState.LOBBY)}
+        ></Button>
+        <Link href="/">
+          <Button text="Return to homescreen"></Button>
+        </Link>
+      </div>
     </main>
   );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,22 @@
 type Props = {
   text: string;
   rounded?: boolean;
+  outline?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: React.FC<Props> = (props) => {
-  const { text, rounded } = props;
+  const { text, rounded, outline } = props;
 
   return (
     <button
       {...props}
-      className={`relative ${rounded ? "rounded-full" : "rounded-md"} w-fit bg-highlight p-4 text-xs
-      text-white drop-shadow`}
+      className={`relative ${rounded ? "rounded-full" : "rounded-md"} w-fit border border-highlight ${outline ? "" : "bg-highlight"} p-4
+      text-xs text-white drop-shadow`}
     >
       {text}
-      <div className="absolute left-0 top-0 size-full rounded-full opacity-5 hover:bg-black" />
+      <div
+        className={`absolute left-0 top-0 size-full ${rounded ? "rounded-full" : "rounded-md"} opacity-5 hover:bg-black`}
+      />
     </button>
   );
 };
